809139c1ae671191827b3e8b8d1f23a3b9fda234
Natali Angelova <natali.v.angelova@gmail.com>
2022-10-29 09:42:50 +0300

    Asynchronous Programming - ex. Bus Stop

--- Asynchronous Programming/exercise/01.Bus-Stop/app.js
+++ Asynchronous Programming/exercise/01.Bus-Stop/app.js
@@ -1,3 +1,39 @@
-function getInfo() {
-    console.log("TODO...");
+async function getInfo() {
+  const busId = document.querySelector("#stopId").value;
+  const url = `http://localhost:3030/jsonstore/bus/businfo/${busId}`;
+
+  const stopName = document.querySelector("#stopName");
+  const allBuses = document.querySelector("#buses");
+
+  try {
+    const res = await fetch(url);
+
+    if (res.status !== 200) {
+      throw new Error("Stop ID not found");
+    }
+
+    const data = await res.json();
+    allBuses.innerHTML = "";
+    stopName.textContent = data.name;
+    Object.entries(data.buses).forEach(bus =>
+      e("li", `Bus ${bus[0]} arrives in ${bus[1]} minutes`, allBuses)
+    );
+  } catch (error) {
+    stopName.textContent = "Error";
+    while (allBuses.firstChild) {
+      allBuses.removeChild(allBuses.firstChild);
+    }
+  }
+
+  function e(type, content, parent) {
+    const element = document.createElement(type);
+    element.textContent = content;
+
+    if (parent) {
+      parent.appendChild(element);
+    }
+    return element;
+  }
 }
+
+module.exports = getInfo;
--- Asynchronous Programming/exercise/01.Bus-Stop/tests/e2e.test.js
+++ Asynchronous Programming/exercise/01.Bus-Stop/tests/e2e.test.js
@@ -1,7 +1,7 @@
-const { chromium } = require('playwright-chromium');
-const { expect } = require('chai');
+const { chromium } = require("../app");
+const { expect } = require("chai");
 
-const host = 'http://localhost:' + 3000; // Application host (NOT service host - that can be anything)
+const host = "http://localhost:" + 3000; // Application host (NOT service host - that can be anything)
 
 const DEBUG = false;
 const slowMo = 500;
@@ -9,36 +9,36 @@ const slowMo = 500;
 const mockData = {
   list: [
     {
-      _id: '1287',
+      _id: "1287",
       buses: { 76: 15, 84: 10, 204: 10, 213: 18, 280: 9, 306: 31, 604: 11 },
-      name: 'Orlov Most sq.',
+      name: "Orlov Most sq.",
     },
     {
-      _id: '1308',
+      _id: "1308",
       buses: { 4: 13, 12: 6, 18: 7 },
-      name: 'St. Nedelya sq.',
+      name: "St. Nedelya sq.",
     },
     {
-      _id: '1327',
+      _id: "1327",
       buses: { 78: 18, 85: 20, 213: 18, 285: 20, 305: 18, 404: 18, 413: 16 },
-      name: 'Central Train Station sq.',
+      name: "Central Train Station sq.",
     },
     {
-      _id: '2334',
+      _id: "2334",
       buses: { 20: 11, 22: 4 },
-      name: 'Centralni Hali',
+      name: "Centralni Hali",
     },
   ],
 };
 
 const endpoints = {
-  catalog: (id) => `/jsonstore/bus/businfo/${id}`,
+  catalog: id => `/jsonstore/bus/businfo/${id}`,
 };
 
 let browser;
 let page;
 
-describe('E2E tests', function () {
+describe("E2E tests", function () {
   this.timeout(6000);
 
   before(async () => {
@@ -54,85 +54,85 @@ describe('E2E tests', function () {
     await page.close();
   });
 
-  describe('List', () => {
-    it('Show bus stop name', async () => {
+  describe("List", () => {
+    it("Show bus stop name", async () => {
       await page.goto(host);
       const data = mockData.list[3];
       const { get } = await handle(endpoints.catalog(data._id));
       get(data);
-      await page.waitForSelector('#stopId');
+      await page.waitForSelector("#stopId");
 
       await page.fill('[type="text"]', data._id);
       await page.click('[type="button"]');
 
-      await page.waitForSelector('#stopName');
+      await page.waitForSelector("#stopName");
 
-      const search = await page.$$eval(`#stopName`, (t) =>
-        t.map((s) => s.textContent)
+      const search = await page.$$eval(`#stopName`, t =>
+        t.map(s => s.textContent)
       );
-      await page.waitForSelector('#buses>li');
+      await page.waitForSelector("#buses>li");
       expect(search[0]).to.contains(data.name);
     });
 
-    it('Match bus stops length', async () => {
+    it("Match bus stops length", async () => {
       await page.goto(host);
       const data = mockData.list[3];
       const { get } = await handle(endpoints.catalog(data._id));
       get(data);
-      await page.waitForSelector('#stopId');
+      await page.waitForSelector("#stopId");
 
       await page.fill('[type="text"]', data._id);
       await page.click('[type="button"]');
 
-      await page.waitForSelector('#stopName');
+      await page.waitForSelector("#stopName");
 
-      const stops = await page.$$eval(`#buses li`, (t) =>
-        t.map((s) => s.textContent)
+      const stops = await page.$$eval(`#buses li`, t =>
+        t.map(s => s.textContent)
       );
-      await page.waitForSelector('#buses>li');
+      await page.waitForSelector("#buses>li");
 
       expect(stops.length).to.equal(2);
     });
 
-    it('Match bus stops length with wrong ID', async () => {
+    it("Match bus stops length with wrong ID", async () => {
       await page.goto(host);
       const data = mockData.list[0];
       const { get } = await handle(endpoints.catalog(data._id));
       get(data);
-      await page.waitForSelector('#stopId');
+      await page.waitForSelector("#stopId");
 
-      await page.fill('[type="text"]', '1000');
+      await page.fill('[type="text"]', "1000");
       await page.click('[type="button"]');
 
-      await page.waitForSelector('#stopName');
+      await page.waitForSelector("#stopName");
 
-      const stops = await page.$$eval(`#buses li`, (t) =>
-        t.map((s) => s.textContent)
+      const stops = await page.$$eval(`#buses li`, t =>
+        t.map(s => s.textContent)
       );
-      await page.waitForSelector('#stopName');
+      await page.waitForSelector("#stopName");
 
       expect(stops.length).to.equal(0);
     });
 
-    it('Show error with wrong ID', async () => {
+    it("Show error with wrong ID", async () => {
       await page.goto(host);
       const data = mockData.list[0];
       const { get } = await handle(endpoints.catalog(data._id));
       get(data);
-      await page.waitForSelector('#stopId');
+      await page.waitForSelector("#stopId");
 
-      await page.fill('[type="text"]', '1000');
+      await page.fill('[type="text"]', "1000");
       await page.click('[type="button"]');
 
-      await page.waitForSelector('#stopName');
+      await page.waitForSelector("#stopName");
 
-      const search = await page.$$eval(`#stopName`, (t) =>
-        t.map((s) => s.textContent)
+      const search = await page.$$eval(`#stopName`, t =>
+        t.map(s => s.textContent)
       );
 
-      await page.waitForSelector('#stopName');
+      await page.waitForSelector("#stopName");
 
-      expect(search[0]).to.contains('Error');
+      expect(search[0]).to.contains("Error");
     });
   });
 });
@@ -143,10 +143,10 @@ async function setupContext(context) {
 
   // Block external calls
   await context.route(
-    (url) => url.href.slice(0, host.length) != host,
-    (route) => {
+    url => url.href.slice(0, host.length) != host,
+    route => {
       if (DEBUG) {
-        console.log('Preventing external call to ' + route.request().url());
+        console.log("Preventing external call to " + route.request().url());
       }
       route.abort();
     }
@@ -164,20 +164,20 @@ function handleContext(context, match, handlers) {
 async function handleRaw(match, handlers) {
   const methodHandlers = {};
   const result = {
-    get: (returns, options) => request('GET', returns, options),
-    get2: (returns, options) => request('GET', returns, options),
-    post: (returns, options) => request('POST', returns, options),
-    put: (returns, options) => request('PUT', returns, options),
-    patch: (returns, options) => request('PATCH', returns, options),
-    del: (returns, options) => request('DELETE', returns, options),
-    delete: (returns, options) => request('DELETE', returns, options),
+    get: (returns, options) => request("GET", returns, options),
+    get2: (returns, options) => request("GET", returns, options),
+    post: (returns, options) => request("POST", returns, options),
+    put: (returns, options) => request("PUT", returns, options),
+    patch: (returns, options) => request("PATCH", returns, options),
+    del: (returns, options) => request("DELETE", returns, options),
+    delete: (returns, options) => request("DELETE", returns, options),
   };
 
   const context = this;
 
   await context.route(urlPredicate, (route, request) => {
     if (DEBUG) {
-      console.log('>>>', request.method(), request.url());
+      console.log(">>>", request.method(), request.url());
     }
 
     const handler = methodHandlers[request.method().toLowerCase()];
@@ -190,7 +190,7 @@ async function handleRaw(match, handlers) {
 
   if (handlers) {
     for (let method in handlers) {
-      if (typeof handlers[method] == 'function') {
+      if (typeof handlers[method] == "function") {
         handlers[method](result[method]);
       } else {
         result[method](handlers[method]);
@@ -234,10 +234,10 @@ function respond(data, options = {}) {
   );
 
   const headers = {
-    'Access-Control-Allow-Origin': '*',
+    "Access-Control-Allow-Origin": "*",
   };
   if (options.json) {
-    headers['Content-Type'] = 'application/json';
+    headers["Content-Type"] = "application/json";
     data = JSON.stringify(data);
   }
 
