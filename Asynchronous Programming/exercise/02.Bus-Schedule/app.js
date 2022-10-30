390b2806bdd4a2afc0af137258031318fb5cd6e7
Natali Angelova <natali.v.angelova@gmail.com>
2022-10-30 20:43:40 +0200

    Asynchronous Programming/exercise/02.Bus-Schedule

--- Asynchronous Programming/exercise/02.Bus-Schedule/app.js
+++ Asynchronous Programming/exercise/02.Bus-Schedule/app.js
@@ -1,17 +1,56 @@
 function solve() {
+  const departBtn = document.querySelector("#depart");
+  const arriveBtn = document.querySelector("#arrive");
 
-    function depart() {
-        console.log('Depart TODO...');
+  const info = document.querySelector("#info span");
+  let id = "depot";
+  const url = `http://localhost:3030/jsonstore/bus/schedule/${id}`;
+
+  async function depart() {
+    try {
+      const response = await fetch(url);
+
+      if (!response.ok) {
+        throw new Error("Invalid bus id");
+      }
+      const data = await response.json();
+
+      departBtn.disabled = true;
+      arriveBtn.disabled = false;
+
+      info.textContent = `Next stop ${data.name}`;
+    } catch (err) {
+      info.textContent = "Error";
+      departBtn.disabled = true;
+      arriveBtn.disabled = true;
     }
+  }
 
-    function arrive() {
-        console.log('Arrive TODO...');
+  async function arrive() {
+    try {
+      const response = await fetch(url);
+
+      if (!response.ok) {
+        throw new Error("Invalid bus id");
+      }
+
+      const data = await response.json();
+
+      departBtn.disabled = false;
+      arriveBtn.disabled = true;
+
+      info.textContent = `Arriving at ${data.name}`;
+    } catch (err) {
+      info.textContent = "Error";
+      departBtn.disabled = true;
+      arriveBtn.disabled = true;
     }
+  }
 
-    return {
-        depart,
-        arrive
-    };
+  return {
+    depart,
+    arrive,
+  };
 }
 
 let result = solve();
