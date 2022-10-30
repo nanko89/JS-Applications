0a3014e012b252a08f65cd2c57ed59d2c16a6b0d
Natali Angelova <natali.v.angelova@gmail.com>
2022-10-30 21:06:21 +0200

    Asynchronous Programming/exercise/03.Forecaster -50/100

--- Asynchronous Programming/exercise/03.Forecaster/app.js
+++ Asynchronous Programming/exercise/03.Forecaster/app.js
@@ -1,5 +1,130 @@
+let town = document.querySelector("#location");
+
+let forecast = document.querySelector("#forecast");
+let current = document.querySelector("#current");
+let upcoming = document.querySelector("#upcoming");
+let submitBtn = document.querySelector("#submit");
+
+const url = "http://localhost:3030/jsonstore/forecaster/locations";
+
+const symbol = {
+  Sunny: "\u2600",
+  "Partly sunny": "\u26c5",
+  Overcast: "\u2601",
+  Rain: "\u2602",
+  Degrees: "\u00b0",
+};
+
 function attachEvents() {
-    console.log("TODO...");
+  submitBtn.addEventListener("click", getWeather);
 }
 
+async function getWeather() {
+  debugger;
+  try {
+    if (!town.value) {
+      throw new Error();
+    }
+
+    if (current.children[1]) {
+      current.children[1].remove();
+      upcoming.children[1].remove();
+    }
+
+    forecast.style.display = "block";
+    upcoming.style.display = "block";
+
+    current.querySelector(".label").textContent = "Current conditions";
+
+    let res = await fetch(url);
+    if (!res.ok) {
+      throw new Error("Error");
+    }
+    let data = await res.json();
+    let currentTown = data.find(d => d.name === town.value);
+    if (!currentTown) {
+      throw new Error("Error, not found name of town!");
+    }
+
+    previewToday(currentTown.code);
+    previewUpcoming(currentTown.code);
+  } catch (error) {
+    current.querySelector(".label").textContent = "Error";
+    upcoming.style.display = "none";
+  }
+
+  async function previewToday(code) {
+    const urlToday = `http://localhost:3030/jsonstore/forecaster/today/${code}`;
+    let res = await fetch(urlToday);
+    if (!res.ok) {
+      throw new Error("Error: " + res.status);
+    }
+
+    let data = await res.json();
+    if (!data) {
+      throw new Error();
+    }
+
+    let divForcast = e("div", "", "forecasts", current);
+    e(
+      "span",
+      `${symbol[data.forecast.condition]}`,
+      "condition symbol",
+      divForcast
+    );
+
+    let spanCondition = e("span", "", "condition", divForcast);
+
+    e("span", `${data.name}`, "forecast-data", spanCondition);
+    e(
+      "span",
+      `${data.forecast.low}${symbol["Degrees"]}/${data.forecast.high}${symbol["Degrees"]}`,
+      "forecast-data",
+      spanCondition
+    );
+    e("span", `${data.forecast.condition}`, "forecast-data", spanCondition);
+  }
+
+  async function previewUpcoming(code) {
+    const urlUpcoming = `http://localhost:3030/jsonstore/forecaster/upcoming/${code}`;
+
+    let res = await fetch(urlUpcoming);
+    if (!res.ok) {
+      throw new Error("Error: " + res.status);
+    }
+
+    let allData = await res.json();
+
+    if (!allData) {
+      throw new Error();
+    }
+    let divForcast = e("div", "", "forecasts-info", upcoming);
+
+    for (const data of allData.forecast) {
+      let spanUpcoming = e("span", "", "upcoming", divForcast);
+
+      e("span", `${symbol[data.condition]}`, "symbol", spanUpcoming);
+
+      e(
+        "span",
+        `${data.low}${symbol["Degrees"]}/${data.high}${symbol["Degrees"]}`,
+        "forecast-data",
+        spanUpcoming
+      );
+
+      e("span", `${data.condition}`, "forecast-data", spanUpcoming);
+    }
+  }
+
+  function e(type, content, className, parent) {
+    const element = document.createElement(type);
+    element.textContent = content;
+    element.className = className;
+
+    if (parent) {
+      parent.appendChild(element);
+    }
+    return element;
+  }
+}
 attachEvents();
--- Asynchronous Programming/exercise/03.Forecaster/tests/e2e.test.js
+++ Asynchronous Programming/exercise/03.Forecaster/tests/e2e.test.js
@@ -1,7 +1,7 @@
-const { chromium } = require('playwright-chromium');
-const { expect } = require('chai');
+const { chromium } = require("playwright-chromium");
+const { expect } = require("chai");
 
-const host = 'http://localhost:3000'; // Application host (NOT service host - that can be anything)
+const host = "http://localhost:3000"; // Application host (NOT service host - that can be anything)
 
 const DEBUG = false;
 const slowMo = 500;
@@ -9,55 +9,55 @@ const slowMo = 500;
 const mockData = {
   location: [
     {
-      code: 'ny',
-      name: 'New York',
+      code: "ny",
+      name: "New York",
     },
   ],
   today: [
     {
       forecast: {
-        condition: 'Sunny',
-        high: '19',
-        low: '8',
+        condition: "Sunny",
+        high: "19",
+        low: "8",
       },
-      name: 'New York, USA',
+      name: "New York, USA",
     },
   ],
   upcomming: [
     {
       forecast: [
         {
-          condition: 'Partly sunny',
-          high: '17',
-          low: '6',
+          condition: "Partly sunny",
+          high: "17",
+          low: "6",
         },
         {
-          condition: 'Overcast',
-          high: '9',
-          low: '3',
+          condition: "Overcast",
+          high: "9",
+          low: "3",
         },
         {
-          condition: 'Overcast',
-          high: '7',
-          low: '3',
+          condition: "Overcast",
+          high: "7",
+          low: "3",
         },
       ],
-      name: 'New York',
+      name: "New York",
     },
   ],
 };
 
 const endpoints = {
-  locations: '/jsonstore/forecaster/locations',
-  today: (code) => `/jsonstore/forecaster/today/${code}`,
-  upcommig: (code) => `/jsonstore/forecaster/upcoming/${code}`,
+  locations: "/jsonstore/forecaster/locations",
+  today: code => `/jsonstore/forecaster/today/${code}`,
+  upcommig: code => `/jsonstore/forecaster/upcoming/${code}`,
 };
 
 let browser;
 let context;
 let page;
 
-describe('E2E tests', function () {
+describe("E2E tests", function () {
   // Setup
   this.timeout(DEBUG ? 120000 : 7000);
   before(
@@ -78,105 +78,102 @@ describe('E2E tests', function () {
   });
 
   // Test proper
-  describe('Forecaster', () => {
-    it('Search location', async () => {
+  describe("Forecaster", () => {
+    it("Search location", async () => {
       const data = mockData.location;
       const city = mockData.today[0];
       const { get } = await handle(endpoints.locations);
       get(data);
 
       await page.goto(host);
-      await page.waitForSelector('#location');
+      await page.waitForSelector("#location");
 
-      await page.fill('#location', `${data[0].name}`);
+      await page.fill("#location", `${data[0].name}`);
       await page.click('input[type="button"]');
 
       const { get2 } = await handle(endpoints.today(`${data.code}`));
       get2(city);
-      await page.waitForSelector('.forecast-data');
+      await page.waitForSelector(".forecast-data");
 
-      const info = await page.$$eval(
-        `#current .condition .forecast-data`,
-        (t) => t.map((s) => s.textContent)
+      const info = await page.$$eval(`#current .condition .forecast-data`, t =>
+        t.map(s => s.textContent)
       );
-      await page.waitForSelector('.forecast-data');
+      await page.waitForSelector(".forecast-data");
 
       expect(info[0]).to.equal(city.name);
     });
 
-    it('Check current condition info', async () => {
+    it("Check current condition info", async () => {
       const data = mockData.location;
       const city = mockData.today[0];
       const { get } = await handle(endpoints.locations);
       get(data);
 
       await page.goto(host);
-      await page.waitForSelector('#location');
+      await page.waitForSelector("#location");
 
-      await page.fill('#location', `${data[0].name}`);
+      await page.fill("#location", `${data[0].name}`);
       await page.click('input[type="button"]');
-      await page.waitForSelector('.forecast-data');
+      await page.waitForSelector(".forecast-data");
 
       const { get2 } = await handle(endpoints.today(`${data.code}`));
       get2(city);
 
-      const info = await page.$$eval(
-        `#current .condition .forecast-data`,
-        (t) => t.map((s) => s.textContent)
+      const info = await page.$$eval(`#current .condition .forecast-data`, t =>
+        t.map(s => s.textContent)
       );
-      await page.waitForSelector('.forecast-data');
+      await page.waitForSelector(".forecast-data");
 
       expect(info[0]).to.equal(city.name);
       expect(info[1]).to.equal(`${city.forecast.low}°/${city.forecast.high}°`);
       expect(info[2]).to.equal(city.forecast.condition);
     });
 
-    it('Check upcomming days', async () => {
+    it("Check upcomming days", async () => {
       const data = mockData.location;
       const city = mockData.upcomming[0];
       const { get } = await handle(endpoints.locations);
       get(data);
 
       await page.goto(host);
-      await page.waitForSelector('#location');
+      await page.waitForSelector("#location");
 
-      await page.fill('#location', `${data[0].name}`);
+      await page.fill("#location", `${data[0].name}`);
       await page.click('input[type="button"]');
 
       const { get2 } = await handle(endpoints.upcommig(`${data.code}`));
       get2(city);
-      await page.waitForSelector('.upcoming');
+      await page.waitForSelector(".upcoming");
 
-      const info = await page.$$eval(
-        `#upcoming .forecast-info .upcoming`,
-        (t) => t.map((s) => s.textContent)
+      const info = await page.$$eval(`#upcoming .forecast-info .upcoming`, t =>
+        t.map(s => s.textContent)
       );
-      await page.waitForSelector('.upcoming');
+      await page.waitForSelector(".upcoming");
 
       expect(info.length).to.equal(city.forecast.length);
     });
 
-    it('Check upcoming day info', async () => {
+    it("Check upcoming day info", async () => {
       const data = mockData.location;
       const city = mockData.upcomming[0];
       const { get } = await handle(endpoints.locations);
       get(data);
 
       await page.goto(host);
-      await page.waitForSelector('#location');
+      await page.waitForSelector("#location");
 
-      await page.fill('#location', `${data[0].name}`);
+      await page.fill("#location", `${data[0].name}`);
       await page.click('input[type="button"]');
 
       const { get2 } = await handle(endpoints.upcommig(`${data.code}`));
       get2(city);
-      await page.waitForSelector('.forecast-data');
+      await page.waitForSelector(".forecast-data");
 
       const info = await page.$$eval(
         `#upcoming .forecast-info .upcoming .forecast-data`,
-        (t) => t.map((s) => s.textContent)
+        t => t.map(s => s.textContent)
       );
-      await page.waitForSelector('.forecast-data');
+      await page.waitForSelector(".forecast-data");
 
       expect(info[0]).to.equal(
         `${city.forecast[0].low}°/${city.forecast[0].high}°`
@@ -189,43 +186,43 @@ describe('E2E tests', function () {
 async function setupContext(context) {
   // Catalog and Details
   await handleContext(context, endpoints.locations, { get: mockData.location });
-  await handleContext(context, endpoints.today('ny'), {
+  await handleContext(context, endpoints.today("ny"), {
     get: mockData.today[0],
   });
-  await handleContext(context, endpoints.upcommig('ny'), {
+  await handleContext(context, endpoints.upcommig("ny"), {
     get: mockData.upcomming[0],
   });
 
-  await handleContext(context, endpoints.details('1001'), {
+  await handleContext(context, endpoints.details("1001"), {
     get: mockData.catalog[0],
   });
-  await handleContext(context, endpoints.details('1002'), {
+  await handleContext(context, endpoints.details("1002"), {
     get: mockData.catalog[1],
   });
-  await handleContext(context, endpoints.details('1003'), {
+  await handleContext(context, endpoints.details("1003"), {
     get: mockData.catalog[2],
   });
 
   await handleContext(
-    endpoints.profile('0001'),
+    endpoints.profile("0001"),
     { get: mockData.catalog.slice(0, 2) },
     context
   );
 
-  await handleContext(endpoints.total('1001'), { get: 6 }, context);
-  await handleContext(endpoints.total('1002'), { get: 4 }, context);
-  await handleContext(endpoints.total('1003'), { get: 7 }, context);
+  await handleContext(endpoints.total("1001"), { get: 6 }, context);
+  await handleContext(endpoints.total("1002"), { get: 4 }, context);
+  await handleContext(endpoints.total("1003"), { get: 7 }, context);
 
-  await handleContext(endpoints.own('1001', '0001'), { get: 1 }, context);
-  await handleContext(endpoints.own('1002', '0001'), { get: 0 }, context);
-  await handleContext(endpoints.own('1003', '0001'), { get: 0 }, context);
+  await handleContext(endpoints.own("1001", "0001"), { get: 1 }, context);
+  await handleContext(endpoints.own("1002", "0001"), { get: 0 }, context);
+  await handleContext(endpoints.own("1003", "0001"), { get: 0 }, context);
 
   // Block external calls
   await context.route(
-    (url) => url.href.slice(0, host.length) != host,
-    (route) => {
+    url => url.href.slice(0, host.length) != host,
+    route => {
       if (DEBUG) {
-        console.log('Preventing external call to ' + route.request().url());
+        console.log("Preventing external call to " + route.request().url());
       }
       route.abort();
     }
@@ -243,20 +240,20 @@ function handleContext(context, match, handlers) {
 async function handleRaw(match, handlers) {
   const methodHandlers = {};
   const result = {
-    get: (returns, options) => request('GET', returns, options),
-    get2: (returns, options) => request('GET', returns, options),
-    post: (returns, options) => request('POST', returns, options),
-    put: (returns, options) => request('PUT', returns, options),
-    patch: (returns, options) => request('PATCH', returns, options),
-    del: (returns, options) => request('DELETE', returns, options),
-    delete: (returns, options) => request('DELETE', returns, options),
+    get: (returns, options) => request("GET", returns, options),
+    get2: (returns, options) => request("GET", returns, options),
+    post: (returns, options) => request("POST", returns, options),
+    put: (returns, options) => request("PUT", returns, options),
+    patch: (returns, options) => request("PATCH", returns, options),
+    del: (returns, options) => request("DELETE", returns, options),
+    delete: (returns, options) => request("DELETE", returns, options),
   };
 
   const context = this;
 
   await context.route(urlPredicate, (route, request) => {
     if (DEBUG) {
-      console.log('>>>', request.method(), request.url());
+      console.log(">>>", request.method(), request.url());
     }
 
     const handler = methodHandlers[request.method().toLowerCase()];
@@ -269,7 +266,7 @@ async function handleRaw(match, handlers) {
 
   if (handlers) {
     for (let method in handlers) {
-      if (typeof handlers[method] == 'function') {
+      if (typeof handlers[method] == "function") {
         handlers[method](result[method]);
       } else {
         result[method](handlers[method]);
@@ -313,10 +310,10 @@ function respond(data, options = {}) {
   );
 
   const headers = {
-    'Access-Control-Allow-Origin': '*',
+    "Access-Control-Allow-Origin": "*",
   };
   if (options.json) {
-    headers['Content-Type'] = 'application/json';
+    headers["Content-Type"] = "application/json";
     data = JSON.stringify(data);
   }
 
