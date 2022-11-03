fbe90953d5bf5fbe1f7e0b73e29fcdf02155bc10
Natali Angelova <natali.v.angelova@gmail.com>
2022-11-03 13:37:50 +0200

    Remote Data and Authentication/exercise/01.Messenger

--- Remote Data and Authentication/exercise/01.Messenger/app.js
+++ Remote Data and Authentication/exercise/01.Messenger/app.js
@@ -1,5 +1,61 @@
-function attachEvents() {
-    console.log('TODO...');
+async function attachEvents() {
+  const submitBtn = document.getElementById("submit");
+  const refreshBtn = document.getElementById("refresh");
+  const message = document.getElementById("messages");
+
+  submitBtn.addEventListener("click", sendInfo);
+  refreshBtn.addEventListener("click", refresh);
+
+  const url = "http://localhost:3030/jsonstore/messenger";
+
+  async function refresh(e) {
+    message.textContent = "";
+    const response = await fetch(url);
+    try {
+      if (!response.ok) {
+        throw new Error();
+      }
+
+      const data = await response.json();
+
+      let allMessage = "";
+      Object.values(data).forEach(
+        m => (allMessage += `${m.author}: ${m.content}\n`)
+      );
+
+      message.textContent = allMessage.trim();
+    } catch (err) {
+      console.error(err);
+    }
+  }
+
+  async function sendInfo(e) {
+    const author = document.querySelector('input[name="author"]');
+    const content = document.querySelector('input[name="content"]');
+
+    const body = JSON.stringify({
+      author: author.value,
+      content: content.value,
+    });
+
+    try {
+      const response = await fetch(url, {
+        method: "post",
+        headers: {
+          "content-type": "applicasion/json",
+        },
+        body,
+      });
+
+      if (!response.ok) {
+        throw new Error();
+      }
+      author.value = "";
+      content.value = "";
+    } catch (err) {
+      console.error(err.message);
+    }
+  }
 }
 
 attachEvents();
