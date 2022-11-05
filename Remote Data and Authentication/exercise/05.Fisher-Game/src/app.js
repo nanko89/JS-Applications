a06cb5206139b4766c67f7110d0dc4947f2e7567
Natali Angelova <natali.v.angelova@gmail.com>
2022-11-05 23:01:32 +0200

    Remote Data and Authentication - Fisher Game - implement login, register and logout

--- Remote Data and Authentication/exercise/05.Fisher-Game/src/app.js
+++ Remote Data and Authentication/exercise/05.Fisher-Game/src/app.js
@@ -1,1 +1,63 @@
-console.log('TODO:// Implement Home functionality');
+window.onload = onLoadHTML();
+window.addEventListener("DOMContentLoaded", onLoadHTML);
+
+document.querySelector("#addForm").addEventListener("submit", createCatch);
+
+function onLoadHTML() {
+  const guestElements = document.querySelector("#guest");
+  const userElements = document.querySelector("#user");
+  const addBtn = document.querySelector(".add");
+  const nameP = document.querySelector("p span");
+
+  //Logout
+  const logoutBtn = document.querySelector("#logout");
+  logoutBtn.addEventListener("click", e => {
+    e.preventDefault();
+    sessionStorage.clear();
+    location.reload();
+  });
+
+  document.querySelectorAll("a").forEach(a => a.classList.remove("active"));
+  document.getElementById("home").classList.add("active");
+
+  const email = sessionStorage.getItem("email");
+  if (email) {
+    userElements.style.display = "inline-block";
+    guestElements.style.display = "none";
+    nameP.textContent = email;
+    addBtn.disabled = false;
+  } else {
+    userElements.style.display = "none";
+    guestElements.style.display = "inline-block";
+    nameP.textContent = "guest";
+    addBtn.disabled = true;
+  }
+}
+
+function createCatch(e) {
+  e.preventDefault();
+  const formData = new FormData(e.target);
+  const data = Object.fromEntries(formData);
+  onCreateCatch(data);
+}
+
+async function onCreateCatch(body) {
+  const url = "http://localhost:3030/data/catches ";
+
+  const headers = getHeader("POST", body);
+  const response = await fetch(url, headers);
+
+  const data = response.json();
+}
+
+function getHeader(method, body) {
+  const token = sessionStorage.getItem("accessToken");
+  return {
+    method: `${method}`,
+    headers: {
+      "Content-Type": "application/json",
+      "X-Authorization": token,
+    },
+    body: JSON.stringify(body),
+  };
+}
--- Remote Data and Authentication/exercise/05.Fisher-Game/src/register.js
+++ Remote Data and Authentication/exercise/05.Fisher-Game/src/register.js
@@ -1,1 +1,79 @@
-console.log('TODO:// Implement Register functionality');
+document
+  .getElementById("register-form")
+  .addEventListener("submit", registerHandler);
+const guestElements = document.querySelector("#guest");
+const userElements = document.querySelector("#user");
+
+const email = sessionStorage.getItem("email");
+
+document.querySelectorAll("a").forEach(a => a.classList.remove("active"));
+document.getElementById("register").classList.add("active");
+
+if (email) {
+  window.location.href = "index.html";
+  userElements.style.display = "inline-block";
+  guestElements.style.display = "none";
+} else {
+  userElements.style.display = "none";
+  guestElements.style.display = "inline-block";
+}
+
+const errorP = document.querySelector("p.notification");
+
+function registerHandler(e) {
+  e.preventDefault();
+
+  const formData = new FormData(e.target);
+  const email = formData.get("email");
+  const password = formData.get("password");
+  const repass = formData.get("rePass");
+
+  // const {email,password,rePass} = Object.fromEntries(formData);
+
+  if (password !== repass) {
+    errorP.textContent = "Error";
+    setTimeout(() => {
+      errorP.textContent = "";
+    }, 2000);
+  } else {
+    onRegister(email, password);
+  }
+}
+
+async function onRegister(email, password) {
+  const url = "http://localhost:3030/users/register";
+  const body = { email, password };
+  const header = getHeader("POST", body);
+
+  try {
+    const response = await fetch(url, header);
+    if (!response.ok) {
+      throw new Error("Error");
+    }
+    const data = response.json();
+    if (data.code !== 200) {
+      throw new Error("Error");
+    }
+
+    sessionStorage.setItem("id", data._id);
+    sessionStorage.setItem("email", data.email);
+    sessionStorage.setItem("accessToken", data.accessToken);
+    window.location = "./index.html";
+  } catch (err) {
+    errorP.textContent = "Error ";
+    setTimeout(() => {
+      errorP.textContent = "";
+    }, 2000);
+    return;
+  }
+}
+
+function getHeader(method, body) {
+  return {
+    method: `${method}`,
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(body),
+  };
+}
