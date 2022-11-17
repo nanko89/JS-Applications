043f6496f4265f22908d0c05d15da2c0af37c0ce
Natali Angelova <natali.v.angelova@gmail.com>
2022-11-17 03:08:08 +0200

    Client Side Rendering/exercise/03.Search-in-List

--- Client Side Rendering/exercise/03.Search-in-List/search.js
+++ Client Side Rendering/exercise/03.Search-in-List/search.js
@@ -1,3 +1,37 @@
+import { html, render } from "../node_modules/lit-html/lit-html.js";
+import { towns } from "./towns.js";
+
+const rootTowns = document.querySelector("#towns");
+const result = document.querySelector("#result");
+const submitBtn = document.querySelector("button");
+let activeTown = [];
+
+submitBtn.addEventListener("click", search);
+renderHtml();
+
 function search() {
-   // TODO
+  const input = document.querySelector("#searchText");
+  const inputTown = input.value;
+  activeTown = towns.filter(t => t.includes(inputTown));
+  let matches = activeTown.length;
+
+  if (input.value === "") {
+    matches = 0;
+  }
+
+  renderHtml();
+
+  result.textContent = `${matches} matches found`;
+}
+
+function renderHtml() {
+  const ul = html` <ul>
+    ${towns.map(t => html`<li class=${isActive(t) ? "active" : ""}>${t}</li>`)}
+  </ul>`;
+
+  render(ul, rootTowns);
+}
+
+function isActive(townName) {
+  return activeTown.includes(townName);
 }
